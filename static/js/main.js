f95dbe2d3bf115e75e50c7bec1d88c4a86a4fca4
Chris Jeakle <cjjeakle@umich.edu>
2018-07-12 01:29:38 -0700

    Added slideshow feature, images to locations page.

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,19 @@
-// JS shared across multiple pages in the site
+function showSlide(slideShowName, displayElementId, thumbnailIndex, desiredImageUrl) {
+    let displayImage = document.getElementById(displayElementId);
+    displayImage.src = desiredImageUrl;
+
+    let thumbnails = document.getElementsByClassName(slideShowName + ' thumbnail');
+    for (let i = 0; i < thumbnails.length; i++) {
+        let thumbnail = thumbnails[i];
+        if (thumbnail.src.replace(window.location.origin, '') === desiredImageUrl) {
+            if (thumbnail.className.indexOf('active') != -1) {
+                displayImage.src = '';
+                thumbnail.className = thumbnail.className.replace(' active', '');
+            } else {
+                thumbnail.className += ' active';
+            }
+        } else {
+            thumbnail.className = thumbnail.className.replace(' active', '');
+        }
+    }
+}
